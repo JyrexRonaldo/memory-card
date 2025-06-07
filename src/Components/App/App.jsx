26c16c470b39c1944d051458a8109077e440fa83
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-06-07 03:22:28 +0000

    Add redirects file

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,35 +2,38 @@ import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import Card from "../Card/Card.jsx";
 
+
+function useData(url) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) {
+          let dataArray = [];
+          for (let i = 0; i < 10; i++) {
+            dataArray.push({
+              name: json.results[i].name,
+              image: json.results[i].image,
+            });
+          }
+          setData(dataArray);
+        }
+      });
+    return () => {
+      ignore = true;
+    };
+  }, [url]);
+  return data;
+}
+
 function App() {
   const results = useData("https://rickandmortyapi.com/api/character");
   const [nameArray, setNameArray] = useState([])
   const [bestScore, setBestScore] = useState(0)
 
-  function useData(url) {
-    const [data, setData] = useState(null);
-    useEffect(() => {
-      let ignore = false;
-      fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
-          if (!ignore) {
-            let dataArray = [];
-            for (let i = 0; i < 10; i++) {
-              dataArray.push({
-                name: json.results[i].name,
-                image: json.results[i].image,
-              });
-            }
-            setData(dataArray);
-          }
-        });
-      return () => {
-        ignore = true;
-      };
-    }, [url]);
-    return data;
-  }
+  
 
   function getRandomIndex() {
     return Math.floor(Math.random() * 10);
